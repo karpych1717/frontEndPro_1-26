3e3eb8f8a86cbea1e454cfb45f576214c3b9c7d8
karpych1717 <admkarpych1717@gmail.com>
2023-03-07 10:40:06 +0200

    now it adds rows

--- homework15/setUpToDoList.js
+++ homework15/setUpToDoList.js
@@ -1,7 +1,42 @@
 'use strict'
 
 function setUpToDoList(_wrapper) {
-    console.log(_wrapper)
+  const form = document.createElement('form')
+  form.classList.add('form')
+
+  const formInput = document.createElement('input')
+  formInput.type = 'text'
+  formInput.classList.add('form-input')
+  form.appendChild(formInput)
+
+  const formButton = document.createElement('button')
+  formButton.type = 'button'
+  formButton.textContent = 'Add'
+  formButton.classList.add('button', 'form-button')
+  form.appendChild(formButton)
+
+  formButton.addEventListener('click', addTask)
+
+  _wrapper.appendChild(form)
+
+  const listDiv = document.createElement('div')
+  listDiv.classList.add('task-list')
+
+  _wrapper.appendChild(listDiv)
+}
+
+function addTask(event) {
+  event.preventDefault()
+
+  const text = document.createElement('span')
+  text.textContent = this.closest('.form').getElementsByClassName('form-input')[0].value
+
+  const newTask = document.createElement('div')
+  newTask.classList.add('task')
+  newTask.appendChild(text)
+
+  this.closest('.list-block').
+    getElementsByClassName('task-list')[0].appendChild(newTask)
 }
 
 export default setUpToDoList
