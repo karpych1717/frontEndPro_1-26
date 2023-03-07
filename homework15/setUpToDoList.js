cd3270b7dc61ac2a590f9d068da7fbb955799775
karpych1717 <admkarpych1717@gmail.com>
2023-03-07 11:43:14 +0200

    final hw15

--- homework15/setUpToDoList.js
+++ homework15/setUpToDoList.js
@@ -3,11 +3,13 @@
 function setUpToDoList(_wrapper) {
   const form = document.createElement('form')
   form.classList.add('form')
+  form.addEventListener('submit', addTask)
 
   const formInput = document.createElement('input')
   formInput.type = 'text'
   formInput.classList.add('form-input')
   form.appendChild(formInput)
+  // formInput.value = 'default text for debug'
 
   const formButton = document.createElement('button')
   formButton.type = 'button'
@@ -28,15 +30,43 @@ function setUpToDoList(_wrapper) {
 function addTask(event) {
   event.preventDefault()
 
-  const text = document.createElement('span')
-  text.textContent = this.closest('.form').getElementsByClassName('form-input')[0].value
-
   const newTask = document.createElement('div')
   newTask.classList.add('task')
-  newTask.appendChild(text)
 
+  const check = document.createElement('input')
+  check.type = 'checkbox'
+  check.classList.add('checkbox')
+
+  check.addEventListener('input', function (event) {
+    this.closest('.task').getElementsByClassName('task-text')[0].
+      classList.toggle('strike-text')
+  })
+
+  newTask.appendChild(check)
+
+  const delButton = document.createElement('img')
+  delButton.src = './waka30px.png'
+  delButton.width = '20'
+  delButton.height = '20'
+  delButton.classList.add('waka')
+  delButton.addEventListener('click', removeTask)
+
+  newTask.appendChild(delButton)
+
+  const text = document.createElement('span')
+  text.textContent = this.closest('.form').getElementsByClassName('form-input')[0].value
+  text.classList.add('task-text')
+  newTask.appendChild(text)
+  
   this.closest('.list-block').
     getElementsByClassName('task-list')[0].appendChild(newTask)
+
+  this.closest('.form').getElementsByClassName('form-input')[0].value = ''
+}
+
+function removeTask (event) {
+  this.closest('.task').remove()
+  // console.log('WAKA WAKA')
 }
 
 export default setUpToDoList
