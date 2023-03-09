76dd7101d86ac3723bd9e028bfe06a6556e3a63a
karpych1717 <admkarpych1717@gmail.com>
2023-03-09 13:21:32 +0200

    less js -> add task-template to html

--- homework15/setUpToDoList.js
+++ homework15/setUpToDoList.js
@@ -1,79 +1,43 @@
 'use strict'
 
 function setUpToDoList (_wrapper) {
-  const form = document.createElement('form')
-  form.classList.add('form')
-  form.addEventListener('submit', addTask)
-
-  const formInput = document.createElement('input')
-  formInput.type = 'text'
-  formInput.classList.add('form-input')
-  form.appendChild(formInput)
+  const formInput = _wrapper.querySelector('.js--form')
+  formInput.addEventListener('submit', addTask)
   // formInput.value = 'default text for debug'
-
-  const submitButton = document.createElement('button')
-  submitButton.type = 'submit'
-  submitButton.textContent = 'Add'
-  submitButton.classList.add('button')
-  form.appendChild(submitButton)
-
-  const resetButton = document.createElement('button')
-  resetButton.type = 'reset'
-  resetButton.textContent = 'Reset'
-  resetButton.classList.add('button')
-  form.appendChild(resetButton)
-
-  _wrapper.appendChild(form)
-
-  const listDiv = document.createElement('div')
-  listDiv.classList.add('task-list')
-
-  _wrapper.appendChild(listDiv)
 }
 
 function addTask (event) {
   event.preventDefault()
 
-  const inputText = this.closest('.form').querySelector('.form-input').value
+  const inputText = this.querySelector('.js--form-input').value
 
-  if (inputText.replaceAll(' ', '') === '') return
+  if (inputText.trim() === '') return
 
-  const newTask = document.createElement('div')
-  newTask.classList.add('task')
+  const newTask = this.closest('.js--list-block').querySelector('.js--task-template')
+    .content.cloneNode(true)
 
-  const check = document.createElement('input')
-  check.type = 'checkbox'
-  check.classList.add('checkbox')
+  const checkbox = newTask.querySelector('.js--checkbox')
+  checkbox.addEventListener('input', strikeTask)
 
-  check.addEventListener('input', function (event) {
-    this.closest('.task').querySelector('.task-text')
-      .classList.toggle('strike-text')
-  })
-
-  newTask.appendChild(check)
-
-  const delButton = document.createElement('img')
-  delButton.src = './waka30px.png'
-  delButton.width = '20'
-  delButton.height = '20'
-  delButton.classList.add('waka')
+  const delButton = newTask.querySelector('.js--waka')
   delButton.addEventListener('click', removeTask)
 
-  newTask.appendChild(delButton)
+  const text = newTask.querySelector('.js--task-text')
+  text.textContent = inputText.trim()
 
-  const text = document.createElement('span')
-  text.textContent = inputText.trimStart()
-  text.classList.add('task-text')
-  newTask.appendChild(text)
+  this.closest('.js--list-block')
+    .querySelector('.js--task-list').appendChild(newTask)
 
-  this.closest('.list-block')
-    .querySelector('.task-list').appendChild(newTask)
+  this.querySelector('.js--form-input').value = ''
+}
 
-  this.closest('.form').querySelector('.form-input').value = ''
+function strikeTask (event) {
+  this.closest('.js--task').querySelector('.js--task-text')
+    .classList.toggle('strike-text')
 }
 
 function removeTask (event) {
-  this.closest('.task').remove()
+  this.closest('.js--task').remove()
   // console.log('WAKA WAKA')
 }
 
