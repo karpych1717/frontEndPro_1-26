54ba7385397951bb7c0c4a02767565844045b462
karpych1717 <admkarpych1717@gmail.com>
2023-04-04 22:03:55 +0300

    released hw 20

--- homework20/main.js
+++ homework20/main.js
@@ -13,7 +13,6 @@ function Student (fullName, birthDay, marks) {
 
 Student.prototype.getAge = function () {
   const today = new Date()
-
   const age = Math.round( (today - this.birthDay) / 1000 / 60 / 60 / 24 / 365.25 )
 
   return age
@@ -62,7 +61,9 @@ Student.prototype.summary = function () {
     presenceAmount++
   }
 
-  const presenceVolume = presenceAmount / lessonsAmount
+  const presenceVolume = lessonsAmount > 0 
+    ? presenceAmount / lessonsAmount
+    : 0
 
   if (avarageMark > 90 && presenceVolume > 0.9) {
     return 'Molodets!'
@@ -75,6 +76,25 @@ Student.prototype.summary = function () {
   return 'Redyska!'
 }
 
-const student1 = new Student({name: 'Vasya', surname: 'Poopkeen'}, new Date('1991-05-08'), [100, 80, 10])
-const student2 = new Student({name: 'Petya', surname: 'Nagibator'}, new Date('1993-02-01'), [100, 90, 100, 99])
-const student3 = new Student({name: 'Seryozha', surname: 'Ivanov'}, new Date('1998-10-7'), [100, 65, 100])
+const student1 = new Student({ name: 'Vasya', surname: 'Poopkeen' }, new Date('1991-05-08'), [ 100, 80, 10 ])
+const student2 = new Student({ name: 'Petya', surname: 'Nagibator' }, new Date('1993-02-01'), [ 100, 90, 100 ])
+const student3 = new Student({ name: 'Seryozha', surname: 'Ivanov' }, new Date('1998-10-07'), [ 100, 65, 100 ])
+
+;[student1, student2, student3]
+  .forEach(student => {
+    for (let i = 0; i < 26; i++) {
+      randomAttend(student)
+    }
+  })
+
+console.log('student1:', student1.getAge(), student1.summary())
+console.log('student2:', student2.getAge(), student2.summary())
+console.log('student3:', student3.getAge(), student3.summary())
+
+function randomAttend (student) {
+  if (Math.random() < 0.1) {
+    student.absent()
+  } else {
+    student.present()
+  }
+}
