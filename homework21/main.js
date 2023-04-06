9701befc21fbe0c4976f2ccfdf1fc52d8ff6fb60
karpych1717 <admkarpych1717@gmail.com>
2023-04-06 12:46:47 +0300

    codestyle fix

--- homework21/main.js
+++ homework21/main.js
@@ -16,7 +16,7 @@ const degToWord = (() => {
     'західний',
     'західний',
     'північно-західний',
-    'північно-західний', 
+    'північно-західний',
     'північний'
   ]
 
@@ -24,7 +24,7 @@ const degToWord = (() => {
     const index = Math.trunc(deg / 22.5)
 
     return sectors[index]
-  } 
+  }
 })()
 
 function main () {
@@ -32,7 +32,7 @@ function main () {
   const wrapper = span.closest('div')
   const pic = wrapper.querySelector('img')
 
-  let path = new URL(
+  const path = new URL(
     '?q=KYIV&units=metric&APPID=5d066958a60d315387d9492393935c19',
     'https://api.openweathermap.org/data/2.5/weather'
   )
@@ -42,23 +42,23 @@ function main () {
 
   document.querySelector('button').addEventListener('click', function () {
     fetch(path, { method: 'GET' })
-    .then(responce => responce.json())
-    .then(data => {
-      weather =
-        `Температура в Києві складає ${data.main.temp} градусів, ` +
-        `атмосферний тиск - ${Math.round(data.main.pressure / 1000 * 760)} мм.рт.ст., ` +
-        `вологість повітря - ${data.main.humidity}%, ` +
-        `швидкість вітру - ${data.wind.speed} м/с, ` +
-        `напрямок вітру - ${degToWord(data.wind.deg)}.`
+      .then(responce => responce.json())
+      .then(data => {
+        weather =
+          `Температура в Києві складає ${data.main.temp} градусів, ` +
+          `атмосферний тиск - ${Math.round(data.main.pressure / 1000 * 760)} мм.рт.ст., ` +
+          `вологість повітря - ${data.main.humidity}%, ` +
+          `швидкість вітру - ${data.wind.speed} м/с, ` +
+          `напрямок вітру - ${degToWord(data.wind.deg)}.`
 
-      iconRef = new URL (
+        iconRef = new URL(
         `/img/w/${data.weather[0].icon}.png`,
         'https://openweathermap.org'
-      )
+        )
 
-      span.textContent = weather
-      pic.src = iconRef.href
-    })
+        span.textContent = weather
+        pic.src = iconRef.href
+      })
   })
 }
 
