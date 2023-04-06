d81a76d4d3c0a13e0df46fcb83fd5fe0b94242a0
karpych1717 <admkarpych1717@gmail.com>
2023-04-06 12:35:49 +0300

    refactoring

--- homework21/main.js
+++ homework21/main.js
@@ -27,33 +27,39 @@ const degToWord = (() => {
   } 
 })()
 
-document.addEventListener('DOMContentLoaded', function () {
+function main () {
+  const span = document.querySelector('span')
+  const wrapper = span.closest('div')
+  const pic = wrapper.querySelector('img')
+
+  let path = new URL(
+    '?q=KYIV&units=metric&APPID=5d066958a60d315387d9492393935c19',
+    'https://api.openweathermap.org/data/2.5/weather'
+  )
+
+  let weather
+  let iconRef
 
   document.querySelector('button').addEventListener('click', function () {
-    const span = document.querySelector('span')
-    const wrapper = span.closest('div')
-    const pic = wrapper.querySelector('img')
-
-    let weather
-    let iconRef = new URL('https://openweathermap.org')
-
-    let path = new URL('?q=KYIV&units=metric&APPID=5d066958a60d315387d9492393935c19',
-      'https://api.openweathermap.org/data/2.5/weather')
-    
     fetch(path, { method: 'GET' })
     .then(responce => responce.json())
     .then(data => {
-      weather = `Температура в Києві складає ${data.main.temp} градусів, ` +
+      weather =
+        `Температура в Києві складає ${data.main.temp} градусів, ` +
         `атмосферний тиск - ${Math.round(data.main.pressure / 1000 * 760)} мм.рт.ст., ` +
         `вологість повітря - ${data.main.humidity}%, ` +
         `швидкість вітру - ${data.wind.speed} м/с, ` +
         `напрямок вітру - ${degToWord(data.wind.deg)}.`
 
-      iconRef = new URL ( `/img/w/${data.weather[0].icon}.png`, iconRef)
-
-      pic.src = iconRef.href
+      iconRef = new URL (
+        `/img/w/${data.weather[0].icon}.png`,
+        'https://openweathermap.org'
+      )
 
       span.textContent = weather
+      pic.src = iconRef.href
     })
   })
-})
+}
+
+document.addEventListener('DOMContentLoaded', main)
