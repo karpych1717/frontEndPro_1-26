e22ef5018a83ad45b183e325b4e5020b35495226
karpych1717 <admkarpych1717@gmail.com>
2023-04-23 22:49:52 +0300

    load js

--- homework24/webpack.config.js
+++ homework24/webpack.config.js
@@ -1,7 +1,14 @@
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
+  mode: 'development',
+  entry: './src/js/index.js',
+  output: { path: path.resolve(__dirname, 'dist') },
   module: {
-    rules: [
-      
-    ]
-  }
+    rules: []
+  },
+  plugins: [
+    new htmlWebpackPlugin({ template: './src/index.html'})
+  ]
 }
