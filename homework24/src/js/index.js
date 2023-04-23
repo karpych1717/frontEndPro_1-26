f5dde90af951f78bfcdc550db43c56f208f4a0f2
karpych1717 <admkarpych1717@gmail.com>
2023-04-23 23:22:00 +0300

    release

--- homework24/src/js/index.js
+++ homework24/src/js/index.js
@@ -1,4 +1,7 @@
 /* globals setUp */
+import '../scss/globalStyles.scss'
+import '../scss/localStyles.scss'
+
 import setUp from './app.js'
 
 window.onload = () => setUp(document.querySelector('.js--app-wrapper'))
--- homework24/webpack.config.js
+++ homework24/webpack.config.js
@@ -3,22 +3,19 @@ const htmlWebpackPlugin = require('html-webpack-plugin')
 const miniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/js/index.js',
   output: { path: path.resolve(__dirname, 'dist') },
   module: {
     rules: [
       {
         test: /\.s[ac]ss$/i,
-        use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader",
-        ],
+        use: [miniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       }
     ],
   },
   plugins: [
     new htmlWebpackPlugin({ template: './src/index.html'}),
+    new miniCssExtractPlugin({ filename: 'index.css' })
   ]
 }
