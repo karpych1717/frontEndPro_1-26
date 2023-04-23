b483eb16574bec1a531dce25eb9c7810281e93dc
karpych1717 <admkarpych1717@gmail.com>
2023-04-23 23:04:30 +0300

    add styles

--- homework24/webpack.config.js
+++ homework24/webpack.config.js
@@ -1,14 +1,17 @@
 const path = require('path')
 const htmlWebpackPlugin = require('html-webpack-plugin')
+const miniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   mode: 'development',
   entry: './src/js/index.js',
   output: { path: path.resolve(__dirname, 'dist') },
   module: {
-    rules: []
+    rules: [],
+    loaders: { test: /\.css$/, loader: 'style-loader|css-loader' }
   },
   plugins: [
-    new htmlWebpackPlugin({ template: './src/index.html'})
+    new htmlWebpackPlugin({ template: './src/index.html'}),
+    new miniCssExtractPlugin({ })
   ]
 }
