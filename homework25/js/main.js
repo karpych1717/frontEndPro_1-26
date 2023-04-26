37066ae43db365e25f49be70bce209494e949a87
karpych1717 <admkarpych1717@gmail.com>
2023-04-27 00:23:47 +0300

    separated functions

--- homework25/js/main.js
+++ homework25/js/main.js
@@ -3,10 +3,10 @@ import setUp from './swapi.js'
 
 document.addEventListener('DOMContentLoaded', () => {
   setUp({
-    form: 'js--swapi_form',
-    pre: 'js--swapi_pre',
-    controller: 'js--swapi_controller',
-    id: 'js--swapi_id',
-    loader: 'js--swapi_load'
+    formClass: 'js--swapi_form',
+    preClass: 'js--swapi_pre',
+    controllerClass: 'js--swapi_controller',
+    idClass: 'js--swapi_id',
+    loaderClass: 'js--swapi_load'
   })
 })
--- homework25/js/swapi.js
+++ homework25/js/swapi.js
@@ -1,77 +1,86 @@
 function setUp (classes) {
-  const { form, pre, controller, id, loader } = classes
+  const { formClass, preClass, controllerClass, idClass, loaderClass } = classes
 
-  const _form = document.querySelector(`.${form}`)
-  const _pre = document.querySelector(`.${pre}`)
-  const _controller = document.querySelector(`.${controller}`)
-  const _id = document.querySelector(`.${id}`)
-  const _loader = document.querySelector(`.${loader}`)
+  const _form = document.querySelector(`.${formClass}`)
+  const _pre = document.querySelector(`.${preClass}`)
+  const _controller = document.querySelector(`.${controllerClass}`)
+  const _id = document.querySelector(`.${idClass}`)
+  const _loader = document.querySelector(`.${loaderClass}`)
 
-  _form.addEventListener('submit', onSubmit)
-
-  async function onSubmit (event) {
+  _form.addEventListener('submit', (event) => {
     event.preventDefault()
+    onSubmit({ _form, _pre, _controller, _id, _loader })
+  })
+}
 
-    _loader.classList.remove('d-none')
+async function onSubmit (elements) {
+  const { _form, _pre, _controller, _id, _loader } = elements
 
-    const formURL = new FormData(_form).get('url')
-    const regEx = /\//
-    const hasSlash = regEx.test(formURL)
+  _loader.classList.remove('d-none')
 
-    if (hasSlash) {
-      const normalizedURL = formURL.trim()
-      const response = await getSwapiData(_form.action, normalizedURL)
-      console.log('response', response)
+  const formURL = new FormData(_form).get('url')
+  const regEx = /\//
+  const hasSlash = regEx.test(formURL)
 
-      showResponse(response, normalizedURL)
-    } else {
-      window.alert('input "/"')
-      _loader.classList.add('d-none')
-    }
-  }
+  if (hasSlash) {
+    const normalizedURL = formURL.trim()
+    const response = await getSwapiData(_form.action, normalizedURL)
+    console.log('response', response)
 
-  async function getSwapiData (start, end) {
-    const response = await fetch(`${start}/${end}`, {
-      method: 'GET'
-    })
-
-    try {
-      return {
-        status: 'success',
-        data: await response.json()
-      }
-    } catch (event) {
-      console.error(event)
-      return {
-        status: 'error',
-        data: null
-      }
-    }
-  }
-
-  function showResponse (response, url) {
-    if (response?.status === 'success') {
-      const getURLInfo = url.split('/')
-      _controller.classList.remove('d-none')
-      _controller.innerHTML = getURLInfo[0]
-      const id = getURLInfo[1]
-
-      if (id) {
-        _id.classList.remove('d-none')
-        _id.innerHTML = id
-      } else {
-        _id.classList.add('d-none')
-      }
-
-      _pre.innerHTML = JSON.stringify(response.data, null, 2)
-    } else {
-      _controller.classList.add('d-none')
-      _id.classList.add('d-none')
-      _pre.innerHTML = JSON.stringify(response, null, 2)
-    }
-
+    showResponse(
+      response,
+      normalizedURL,
+      { _pre, _controller, _id, _loader }
+    )
+  } else {
+    window.alert('input "/"')
     _loader.classList.add('d-none')
   }
+}
+
+async function getSwapiData (start, end) {
+  const response = await fetch(`${start}/${end}`, {
+    method: 'GET'
+  })
+
+  try {
+    return {
+      status: 'success',
+      data: await response.json()
+    }
+  } catch (event) {
+    console.error(event)
+    return {
+      status: 'error',
+      data: null
+    }
+  }
+}
+
+function showResponse (response, url, elements) {
+  const { _pre, _controller, _id, _loader } = elements
+
+  if (response?.status === 'success') {
+    const getURLInfo = url.split('/')
+    _controller.classList.remove('d-none')
+    _controller.innerHTML = getURLInfo[0]
+    const id = getURLInfo[1]
+
+    if (id) {
+      _id.classList.remove('d-none')
+      _id.innerHTML = id
+    } else {
+      _id.classList.add('d-none')
+    }
+
+    _pre.innerHTML = JSON.stringify(response.data, null, 2)
+  } else {
+    _controller.classList.add('d-none')
+    _id.classList.add('d-none')
+    _pre.innerHTML = JSON.stringify(response, null, 2)
+  }
+
+  _loader.classList.add('d-none')
 }
 
 export default setUp
